f3c616c5c887c07dbca7b982c6f13706f30ef5f2
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-06-07 01:37:11 -0500

    Ejemplo Arreglos

--- src/main/java/EjemploArreglos.java
+++ src/main/java/EjemploArreglos.java
@@ -1,37 +1,24 @@
 public class EjemploArreglos {
     public static void main(String[] args) {
-        // declarar arreglo
-        int[] numeros;
-        int[] numeros_2 = new int[3];
+
+        int[] numeros = new int[4];
+
+        numeros[0] = 1;
+        numeros[1] = Integer.valueOf("11");
+        numeros[2] = 111;
+        numeros[3] = -1111;
 
 
-        // declarar con tipo primitivo
-        //  Producto[] productos = new Producto[3];
-        String[] nombres = new String[5];
+        int i = numeros[0];
+        int j = numeros[1];
+        int k = numeros[2];
+        int l = numeros[numeros.length -1 ];
 
-        // posiciones
-        numeros_2[0] = 1;
-        numeros_2[1] = 2;
-        numeros_2[2] = 3;
 
-        // productos[0] = new Producto("mesa comedor");
-        // productos[1] = new Producto("tv sony LED");
-        // productos[2] = new Producto("Bicicleta oxford");
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
 
-        // instanciarlo directamente
-        int[] numeros_directo = {1,2,3};
-
-        String[] nombres_test = {"Andres", "Maria", "Jose", "Isabel", "Juan"};
-
-        for(int i =0; i< nombres_test.length; i++){
-            System.out.println("nombre " + i + "=" + nombres_test[i]);
-        }
-
-        System.out.println();
-        System.out.println("usando for each");
-        System.out.println();
-        for(String nom:nombres_test){
-            System.out.println("nombre = " + nom);
-        }
     }
 }
---
+++ src/main/java/Intro_Arreglos.java
@@ -0,0 +1,37 @@
+public class Intro_Arreglos {
+    public static void main(String[] args) {
+        // declarar arreglo
+        int[] numeros;
+        int[] numeros_2 = new int[3];
+
+
+        // declarar con tipo primitivo
+        //  Producto[] productos = new Producto[3];
+        String[] nombres = new String[5];
+
+        // posiciones
+        numeros_2[0] = 1;
+        numeros_2[1] = 2;
+        numeros_2[2] = 3;
+
+        // productos[0] = new Producto("mesa comedor");
+        // productos[1] = new Producto("tv sony LED");
+        // productos[2] = new Producto("Bicicleta oxford");
+
+        // instanciarlo directamente
+        int[] numeros_directo = {1,2,3};
+
+        String[] nombres_test = {"Andres", "Maria", "Jose", "Isabel", "Juan"};
+
+        for(int i =0; i< nombres_test.length; i++){
+            System.out.println("nombre " + i + "=" + nombres_test[i]);
+        }
+
+        System.out.println();
+        System.out.println("usando for each");
+        System.out.println();
+        for(String nom:nombres_test){
+            System.out.println("nombre = " + nom);
+        }
+    }
+}
